237707ba352bb1550ca683524eeced6e2fd0a5bb
Tunde Akinyanmi <tundebabzy@gmail.com>
2024-03-16 20:06:04 +0100

    add job traveler doctype

---
+++ job_traveler/job_traveler/doctype/job_traveler/test_job_traveler.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, tundebabzy@gmail.com and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestJobTraveler(FrappeTestCase):
+	pass
